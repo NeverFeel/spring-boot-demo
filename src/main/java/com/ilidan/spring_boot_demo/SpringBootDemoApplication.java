c85137e6a5ac4ea90145862ff0ef85b0ff8b536f
Ilidan_Y <smile_yangy@163.com>
2019-06-17 20:47:56 +0800

    测试ApplicationRunner

--- src/main/java/com/ilidan/spring_boot_demo/SpringBootDemoApplication.java
+++ src/main/java/com/ilidan/spring_boot_demo/SpringBootDemoApplication.java
@@ -2,10 +2,15 @@ package com.ilidan.spring_boot_demo;
 
 import com.ilidan.spring_boot_demo.config.DataSourceConfig;
 import com.ilidan.spring_boot_demo.factory.DefaultYamlPropertySourceFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+
+import java.applet.AppletContext;
 
 @PropertySource(value = {"classpath:config/application-redis.yml","classpath:config/application-redis2.yml"},
         ignoreResourceNotFound = false, encoding = "UTF-8", name = "application-redis.yml",
@@ -16,6 +21,16 @@ public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+    /**
+     * 该方法会在应用启动后调用
+     * @param context 上下文对象
+     * @return
+     */
+    @Bean
+    public ApplicationRunner runner(WebServerApplicationContext context){
+        return args -> System.out.println("------------->"+context.getWebServer().getClass().getName());
     }
 
 }
